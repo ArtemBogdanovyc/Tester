a7d0ca7cbb0a61cd0b054937917fc7ad519f20de
Artem <kainqlk@gmail.com>
2021-06-06 11:41:30 +0300

    Update

--- TestWork/Stater.java
+++ TestWork/Stater.java
@@ -1,9 +1,9 @@
 package TestWork;
 
-import java.io.IOException;
+
 
 public class Stater {
-    public static void main(String args[]){
+    public static void main(String[] args){
         CollectionOfFigurs col = new CollectionOfFigurs();
         System.out.println(col.getColect());
 
